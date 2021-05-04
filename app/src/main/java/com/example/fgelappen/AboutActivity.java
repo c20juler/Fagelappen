baa54311cf1f0a30a3005c43dec2291ec0bafe83
c20juler <c20juler@student.his.se>
2021-05-04 17:25:57 +0200

    Added secondActivity (for detailed info about birds...) Also added buttons to navigate between Main and About-activites.

--- app/src/main/java/com/example/fgelappen/AboutActivity.java
+++ app/src/main/java/com/example/fgelappen/AboutActivity.java
@@ -2,7 +2,10 @@ package com.example.fgelappen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button button = findViewById(R.id.close_about);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fgelappen/MainActivity.java
+++ app/src/main/java/com/example/fgelappen/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.start);
+        Button button = findViewById(R.id.about_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
