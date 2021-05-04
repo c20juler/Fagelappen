d046631caa969ea78dbd80867c9a218dc7b4e665
c20juler <c20juler@student.his.se>
2021-05-04 19:06:06 +0200

    Added attribution to Talgoxe image in activity_about

--- app/src/main/java/com/example/fgelappen/AboutActivity.java
+++ app/src/main/java/com/example/fgelappen/AboutActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -21,5 +23,9 @@ public class AboutActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        TextView t2 = (TextView) findViewById(R.id.talgoxe_attribution);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 }
