a6801236d50697e2498e6125aa924b8ebf3de9d8
c20juler <c20juler@student.his.se>
2021-05-06 10:53:49 +0200

    Added arraylist and arrayadapter, created layout file for list item and set adapter on listview.

--- app/src/main/java/com/example/fgelappen/MainActivity.java
+++ app/src/main/java/com/example/fgelappen/MainActivity.java
@@ -6,14 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Bird> listData;
+    private ArrayAdapter<Bird> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Add welcome splash screen...(with welcome msg and bird picture...)
+
+        listData = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, R.layout.list_item, listData);
+
+        ListView my_listView=(ListView) findViewById(R.id.list_view);
+        my_listView.setAdapter(adapter);
+
+
+
+
 
         Button button = findViewById(R.id.about_button);
         button.setOnClickListener(new View.OnClickListener() {
